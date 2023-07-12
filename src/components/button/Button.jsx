ac110e250daec75d9762ba6c98ad57e4d742f52c
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-07-12 08:32:30 +0530

    refractor: landing structure

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({text}) => {
   return (
     <div>
-        <button>text</button>
+        <button>{text}</button>
     </div>
   )
 }
--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import "./landing.css";
+import Button from "../../components/button/Button";
 
 const LandingPage = () => {
   return (
     <div className="landing-container">
-      <div className="landing-content">this is div</div>
-      <div>
+      <div className="landing-content">
+        <h1>
+          <span>My</span>Website
+        </h1>
+        <div>
+          <h2>Meet your modern</h2>
+          <span>Habit Tracker App</span>
+          <p>
+            Manage your daily tasks and workflow in a modeern way and boost your
+            efficiency without efforts.
+          </p>
+        </div>
+        <div>
+          <Button text={"Join Now"} />
+          <p>Already have an account?</p>
+        </div>
+      </div>
+      <div className="image-container">
         <img
           id="landing-img"
           src="https://res.cloudinary.com/dgoldjr3g/image/upload/v1689063164/NegProjects/HabitTracker/undraw_activity_tracker_re_2lvv_hqgpwg.svg"
