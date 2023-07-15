586c77dd8837af6e0e2c1da417e42830c9b69f62
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-07-15 10:29:52 +0530

    chores

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing/LandingPage";
-import LoginPage from "./pages/login/LoginPage";
+import Sidebar from "./components/sidebar/Sidebar";
+import Header from "./components/header/Header";
+import HabitCard from "./components/HabitCard/HabitCard";
+import CountCard from "./components/countCard/CountCard";
+import Profile from "./components/Profile/Profile";
+import NewHabit from "./components/newHabit/NewHabit";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      {/* <Header /> */}
+      {/* <HabitCard /> */}
+      {/* <CountCard />
+      <Profile /> */}
+      {/* <Sidebar /> */}
+      <NewHabit />
+      {/* <Routes>
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
--- src/components/countCard/CountCard.jsx
+++ src/components/countCard/CountCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./countCard.css";
+import "../Profile/Profile.css";
 
 const CountCard = () => {
   return (
